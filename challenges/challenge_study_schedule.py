02296304399cf740f3f5a33b6857ebadc4156472
Crisna Bezerra <crisnabto@gmail.com>
2023-03-23 17:16:46 -0300

    Requisito 1: cria funcao study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,16 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    max_count = 0
+    count = 0
+
+    for period in permanence_period:
+        if not isinstance(period[0], int) or not isinstance(period[1], int):
+            return None
+        if period[0] <= target_time <= period[1]:
+            count += 1
+        if count > max_count:
+            max_count = count
+
+    return max_count
