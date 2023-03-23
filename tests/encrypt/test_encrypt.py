f9a74633556081ba5fa27f01f2818afe4e4a6c97
Crisna Bezerra <crisnabto@gmail.com>
2023-03-23 17:57:00 -0300

    Testa funcao de criptografia encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("awesome algorithms!", 0) == "!smhtirogla emosewa"
+
+    assert encrypt_message("awesome algorithms!", 5) == "osewa_!smhtirogla em"
+
+    with pytest.raises(TypeError):
+        encrypt_message("awesome algorithms!", "not a number")
