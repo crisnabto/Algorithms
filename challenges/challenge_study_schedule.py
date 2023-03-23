7f024faff20d9aa250cfdc77fc114f72da9b4643
Crisna Bezerra <crisnabto@gmail.com>
2023-03-23 17:26:18 -0300

    Refatora a funcao study_schedule para diminuir a complexidade

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    if target_time is None:
+    if target_time is None or not all(
+        isinstance(period[0], int) and isinstance(period[1], int)
+            for period in permanence_period):
         return None
 
     max_count = 0
     count = 0
 
     for period in permanence_period:
-        if not isinstance(period[0], int) or not isinstance(period[1], int):
-            return None
         if period[0] <= target_time <= period[1]:
             count += 1
         if count > max_count:
