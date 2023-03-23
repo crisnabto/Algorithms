d050d82a40071778b03e2a07485a7448206207eb
Crisna Bezerra <crisnabto@gmail.com>
2023-03-23 19:16:32 -0300

    Requisito 3: cria funcao is_palindrome_revursive - verifica se a palavra e palindroma utilizando solucao recursiva

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(word, low_index, high_index):  # (reviver, 0, 6)
+    if word == '':
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    else:
+        return False
