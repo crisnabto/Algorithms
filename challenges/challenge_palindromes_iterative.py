136251c3c9be7f42739c8a89b83d88853a52e7c1
Crisna Bezerra <crisnabto@gmail.com>
2023-03-23 22:44:03 -0300

    Requisito 6: cria funcao is_palindrome_iterative usando solucao iterativa

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,14 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+
+    low_index = 0
+    high_index = len(word) - 1
+
+    while low_index < high_index:
+        if word[low_index] == word[high_index]:
+            low_index += 1
+            high_index -= 1
+        else:
+            return False
+    return True
