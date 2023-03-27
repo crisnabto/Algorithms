d984424e87317a56a357e5c2bad52a5de226bb1d
Crisna Bezerra <crisnabto@gmail.com>
2023-03-27 17:34:33 -0300

    Requisito 5: cria a funcao find_duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,12 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if nums is None or type(nums) == 'str':
+        return False
+    ran = len(nums)
+    sort = sorted(nums)
+    num = 0
+    for a in range(0, ran - 1):
+        if sort[a] == sort[a + 1]:
+            num = sort[a]
+    if num <= 0:
+        return False
+    return num
